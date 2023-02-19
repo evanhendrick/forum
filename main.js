fbdd6d393de5aea8e4c368c5ff5fe00ad71c5a7f
Evan Hendrick <evanjh1996@gmail.com>
2023-02-19 14:24:52 -0500

    new commit

--- main.js
+++ main.js
@@ -1,58 +1,64 @@
 // your code here
+//post variables
 const post = document.getElementById('submit').addEventListener('click', function(){
   let name = document.getElementById('name').value;
   let message = document.getElementById('message').value;
-  // name.innerHTML['style'].fontWeight = 'bold';
-  let posts = document.getElementById('posted') // points to ul so posts = ul
-  let li1 = document.createElement('li');
-  li1.innerHTML = message
-  li2 = document.createElement('li')
-  li2.innerHTML ='Posted By: '+ name
-  li1.style.listStyleType = 'none';
-  li2.style.listStyleType = 'none';
-  posts.append(li1, li2);
- 
+  let posts = document.getElementById('posted') // points to ul aka posts = ul
+  let postText = document.createElement('li');
+  let lineBreak = document.createElement('hr')
+  let postName = document.createElement('li')
+  
+  // posting function
+  postText.innerHTML = message
+  postName.innerHTML ='Posted By: '+ name
+  postText.style.listStyleType = 'none';
+  postName.style.listStyleType = 'none';
+  let del = document.createElement('button');
+  del.innerHTML = 'delete';
+  posts.append(postText, postName, del);
   
   //create buttons for DOM to retrieve
+  let upvote = document.createElement('button');
+  let downvote = document.createElement('button');
+  upvote.innerHTML = 'like';
+  downvote.innerHTML = 'dislike';
+  posts.append(upvote, downvote);
   
-  // let upvote = document.createElement('button');
-  // let downvote = document.createElement('button');
-  // upvote.innerHTML = 'like';
-  // downvote.innerHTML = 'dislike';
-  // posts.append(upvote, downvote);
-  
-  //delete button
-
-
-  // var del = document.createElement('button');
-  // del.innerHTML = 'delete'
-  // posts.append(del)
-  // del.addEventListener('click', function (){
-  //   // posts.remove(li1.innerHTML, li2.innerHTML)
-  // })
-
 
   //upvote function
-
-  // upvote.addEventListener('click', function(){
-  //   let userLike = '';
-  //   if (userLike.length=0){
-  //     userLike = document.createElement('div')
-  //     userLike.innerText = name +' liked this'
-  //     posts.append(userLike);
-  //   } else {
-  //     // posts.remove(userLike)
-  //   }
+  let likes = 0;
+  let dislikes = 0;
+  upvote.addEventListener('click', function(){
+    likes +=1;
+    if (likes <= 1){
+      upvote.innerHTML = likes + ' like!'
+    } else {
+      upvote.innerHTML = likes + ' likes!'
+    }
    
-  // })
+  })
 
-  //delete button
+  //downvote function
+  downvote.addEventListener('click', function(){
+    dislikes +=1;
+    if (dislikes <= 1){
+      downvote.innerHTML = dislikes + ' dislike!'
+    } else {
+      downvote.innerHTML = dislikes + ' dislikes!'
+    }
+  })
+
+  //delete post function
  
+  del.addEventListener('click', function(){
+    postText.remove();
+    postName.remove();
+    del.remove();
+    upvote.remove();
+    downvote.remove();
+    lineBreak.remove();
+  })
+  
 
+posts.append(lineBreak)
 })
-
-  //add upvote and downvote button
-
-
-  //delete button
-  //edit button
